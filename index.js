41ceae92fbb9209c118650d6046c8857b8aba598
sebTeam4 <sebastianmuramara@gmail.com>
2024-07-25 22:46:03 +0300

    enable get weather for the specified city

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 1304;
 const generalAPI_URL = "https://api.airvisual.com/v2/";
 const yourTokenKey = "75f7e8d9-099d-4b5e-a497-a3d28e99485d";
+let iconIndex;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -55,7 +56,48 @@ app.post("/getCities", async (req,res) => {
     }catch(error){
         res.render("index.ejs",{err: error.data.message});
     }
-})
+});
+
+app.post("/getWeather", async (req,res) => {
+    try {
+        const response = await axios.get(generalAPI_URL+"city?city="+req.body["city"]+"&state="+req.body["state"]+
+            "&country="+req.body["country"]+"&key="+yourTokenKey);
+        switch (response.data.data.current.weather.ic) {
+            case "01d": iconIndex = "clear sky (day)"; break;
+
+            case "01n": iconIndex = "clear sky (night)"; break;
+
+            case "02d": iconIndex = "few clouds (day)"; break;
+
+            case "02n": iconIndex = "few clouds (night)"; break;
+
+            case "03d": iconIndex = "scattered clouds"; break;
+
+            case "04d": iconIndex = "broken clouds"; break;
+
+            case "09d": iconIndex = "shower rain"; break;
+
+            case "10d": iconIndex = "rain (day time)"; break;
+
+            case "10n": iconIndex = "rain (night time)"; break;
+
+            case "11d": iconIndex = "thunderstorm"; break;
+
+            case "13d": iconIndex = "snow"; break;
+
+            case "50d": iconIndex = "mist"; break;
+
+            default: iconIndex = "N/A"; break;
+        }
+        res.render("index.ejs",{
+            wthData: response.data.data,
+            cityName: req.body["city"],
+            icon: iconIndex
+        });
+    } catch (error) {
+        res.render("index.ejs",{err: error.data.message});
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
