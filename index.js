a3db958e98458f36c59a6c28b10b7781b4c95772
sebTeam4 <sebastianmuramara@gmail.com>
2024-07-25 15:56:15 +0300

    add forms and get country list from api

--- index.js
+++ index.js
@@ -13,6 +13,36 @@ app.get("/", (req,res) => {
     res.render("index.ejs");
 });
 
+app.get("/country", async (req,res) => {
+    try{
+        const response = await axios.get(generalAPI_URL+"countries?key="+yourTokenKey);
+        console.log(response.data[0]);
+        res.render("index.ejs",{cList: response.data.data});
+    }catch(error){
+        res.render("index.ejs",{err: error.data.message});
+    }
+});
+
+app.get("/state", async (req,res) => {
+    res.render("index.ejs",{getStateList: true});
+    // try{
+    //     const response = await axios.get(generalAPI_URL+"states?country={{COUNTRY_NAME}}&key="+yourTokenKey);
+    //     res.render("index.ejs",{cList: response.data});
+    // }catch(error){
+    //     res.render("index.ejs",{cList: error.data.message});
+    // }
+});
+
+app.get("/city", async (req,res) => {
+    res.render("index.ejs",{getCityList: true});
+    // try{
+    //     const response = await axios.get(generalAPI_URL+"cities?state={{STATE_NAME}}&country={{COUNTRY_NAME}}&key="+yourTokenKey);
+    //     res.render("index.ejs",{cList: response.data});
+    // }catch(error){
+    //     res.render("index.ejs",{cList: error.data.message});
+    // }
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
