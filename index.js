9ed7afed521457facecff40e88cbda8b98f5f57a
sebTeam4 <sebastianmuramara@gmail.com>
2024-09-14 11:56:15 +0300

    hide the IQAIR API key

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import env from "dotenv";
 
 const app = express();
 const port = 1304;
+env.config();
+
 const generalAPI_URL = "https://api.airvisual.com/v2/";
-const yourTokenKey = "75f7e8d9-099d-4b5e-a497-a3d28e99485d";
+const yourTokenKey = process.env.IQAIR_TOKENKEY;
 let iconIndex;
 
 app.use(bodyParser.urlencoded({ extended: true }));
